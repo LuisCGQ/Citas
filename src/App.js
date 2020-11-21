8c107771209b3bf4d8d9b70a56539816b61c1166
Luis García <aig.luis@gmail.com>
2020-11-21 17:11:35 -0600

    Agregada validacion del formulario

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React, { Fragment, fragment } from "react";
+import Formulario from "./components/formulario";
+
 function App() {
-  return <h1>Administrador de Pacientes</h1>;
+  return (
+    <Fragment>
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">2</div>
+        </div>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/formulario.js
@@ -0,0 +1,116 @@
+import React, { Fragment, useState } from "react";
+import uuid from "uuid/dist/v4";
+
+const Formulario = () => {
+  //Crear state de Citas
+  const [cita, actualizarCita] = useState({
+    mascota: "",
+    propietario: "",
+    fecha: "",
+    hora: "",
+    sintomas: "",
+  });
+
+  const [error, actualizarError] = useState(false);
+
+  const actualizarState = (e) => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //Extraer valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  //Cuando el usuario presiona agendar cita
+  const submitCita = (e) => {
+    e.preventDefault();
+
+    //Validar
+    if (
+      mascota.trim() === "" ||
+      propietario.trim() === "" ||
+      fecha.trim() === "" ||
+      hora.trim() === "" ||
+      sintomas.trim() === ""
+    ) {
+      actualizarError(true);
+      return;
+    }
+
+    //Eliminar error
+    actualizarError(false);
+
+    //Asignar ID
+    cita.id = uuid();
+    console.log(cita);
+
+    //Crear la Cita
+
+    //Reiniciar Form
+  };
+
+  return (
+    <Fragment>
+      <h2>Crear Cita</h2>
+
+      {error ? (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      ) : null}
+      <form onSubmit={submitCita}>
+        <label>Nombre Mascota</label>
+        <input
+          type="text"
+          name="mascota"
+          className="u-full-width"
+          placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
+        />
+
+        <label>Nombre Dueño</label>
+        <input
+          type="text"
+          name="propietario"
+          className="u-full-width"
+          placeholder="Nombre Dueño"
+          onChange={actualizarState}
+          value={propietario}
+        />
+
+        <label>Fecha</label>
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
+
+        <label>Nombre Hora</label>
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
+
+        <label>Sintomas</label>
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+
+        <button type="submit" className="u-full-width button-primary">
+          Agregar cita
+        </button>
+      </form>
+    </Fragment>
+  );
+};
+
+export default Formulario;
